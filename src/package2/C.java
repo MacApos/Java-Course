37ba44602088ae01b8624ab7a6df0eeeb0724a64
MacApos <maciejapostol98@gmail.com>
2023-06-15 18:58:25 +0200

    commit

---
+++ src/package2/C.java
@@ -0,0 +1,13 @@
+
+
+package package2;
+
+public class C {
+    public static void main(String[] args) {
+
+        String deafultMessage = "This is default";
+        System.out.println(deafultMessage);
+
+
+    }
+}
